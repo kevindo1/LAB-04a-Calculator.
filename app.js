d21b1b45b9e0e6a47c578d54f8e880d5a99c18e8
Kevin Do <kevind0501@gmail.com>
2021-09-30 14:10:24 -0700

    added JS for addition function

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 // import functions and grab DOM elements
+const addInput1 = document.getElementById('add-input-1');
+const addInput2 = document.getElementById('add-input-2');
+const addOutput = document.getElementById('add-btn');
+const addAnswer = document.getElementById('add-answer');
 
 // initialize global state
 
@@ -6,3 +10,7 @@
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+addOutput.addEventListener('click', () => {
+    console.log('clicked');
+});
